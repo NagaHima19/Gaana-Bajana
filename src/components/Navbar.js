d766c6cf5d62fabb23d9e6586dbff23f212b35d8
NagaHima Maddineni <maddineninagahima19@gmail.com>
2024-08-14 09:53:21 +0530

    music

---
+++ src/components/Navbar.js
@@ -0,0 +1,130 @@
+import React, { useContext } from "react";
+
+import { Link } from "react-router-dom";
+import { MusicContext } from "../Context";
+import PinnedMusic from "./PinnedMusic";
+import LikedMusic from "./LikedMusic";
+
+const Navbar = ({ keyword, handleKeyPress, setKeyword, fetchMusicData }) => {
+  const musicContext = useContext(MusicContext);
+  const likedMusic = musicContext.likedMusic;
+  const pinnedMusic = musicContext.pinnedMusic;
+  const setResultOffset = musicContext.setResultOffset;
+  return (
+    <>
+      <nav className="navbar navbar-dark navbar-expand-lg bg-info sticky-top">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            <i className="bi bi-music-note-list mx-3"></i> Gaana Bajana
+          </Link>
+          <div>
+            <button
+              type="button"
+              data-bs-toggle="modal"
+              data-bs-target="#exampleModal"
+              className="btn btn-success btn-sm mx-1"
+            >
+              <i className="bi bi-pin-angle-fill"></i> {pinnedMusic.length}
+            </button>
+            <button
+              type="button"
+              data-bs-toggle="modal"
+              data-bs-target="#likedMusicModal"
+              className="btn btn-danger btn-sm mx-1"
+            >
+              <i className="bi bi-heart-fill"></i> {likedMusic.length}
+            </button>
+          </div>
+
+          <div
+            className="collapse navbar-collapse d-flex justify-content-center"
+            id="navbarSupportedContent"
+          >
+            <input
+              value={keyword}
+              onChange={(event) => setKeyword(event.target.value)}
+              onKeyDown={handleKeyPress}
+              className="form-control me-2 w-75"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button
+              onClick={() => {
+                setResultOffset(0);
+                fetchMusicData();
+              }}
+              className="btn btn-success"
+            >
+              Search
+            </button>
+            <a
+                target="_blank"
+                rel="noreferrer"
+                className="btn btn-dark m-3"
+                href="https://amithkomaravelli.github.io/GaanaBajana"
+              >
+                <i className="bi bi-github mx-2"></i>Github Link
+              </a>
+          </div>
+        </div>
+      </nav>
+
+      <div
+        className="modal fade modal-xl"
+        id="exampleModal"
+        tabIndex={1}
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="exampleModalLabel">
+                Pinned Music
+              </h1>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              />
+            </div>
+            <div className="modal-body">
+              <PinnedMusic />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div
+        className="modal fade modal-xl"
+        id="likedMusicModal"
+        tabIndex={1}
+        aria-labelledby="likedMusicModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="likedMusicModalLabel">
+                Liked Music
+              </h1>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              />
+            </div>
+            <div className="modal-body">
+              <LikedMusic />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
